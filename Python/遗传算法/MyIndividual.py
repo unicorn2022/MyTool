4ff34aacfb4b285897f1615d722e18959c640804
Dong Jiaxin <496300118@qq.com>
2024-05-17 23:09:43 +0800

    sync

--- Python/遗传算法/MyIndividual.py
+++ Python/遗传算法/MyIndividual.py
@@ -10,18 +10,27 @@ map_config = config["Map_Config"]
 UAV_config = config["UAV_Config"]
 file.close()
 '''创建地图'''
+# 地图大小
 map_width = map_config["width"]
 map_height = map_config["height"]
+# 物资权重
 if map_config["rand_weight"] == True:
     map_weight = np.random.random((map_width, map_height))
 else:
     map_weight = np.ones((map_width, map_height))
+# 飞行时间
+map_time = np.zeros((map_width, map_height))
+for i in range(map_width):
+    for j in range(map_height):
+        map_time[i][j] = np.random.randint(1, 5)
+        if np.random.rand() < 0.1:
+            map_time[i][j] = -1
+# 安全距离
 safe_distance = map_config["safe_distance"]
 '''无人机属性'''
 choice = [0, 1, 2, 3] # 0左转, 1前进, 2右转, 3后退
 UAV_num = UAV_config["num"]
 UAV_search_time = UAV_config["search_time"]
-UAV_search_cost = UAV_config["search_cost"]
 UAV_search_radius = UAV_config["search_radius"]
 UAV_start_position = UAV_config["start_position"]
 
@@ -75,7 +84,7 @@ class Individual:
             self.gene_start_index = []
             for i in range(UAV_num):
                 self.gene_start_index.append(self.gene_count)
-                self.gene_count += UAV_search_time // UAV_search_cost[i]
+                self.gene_count += UAV_search_time
             self.gene_start_index.append(self.gene_count)
             self.genotype = [Gene() for _ in range(self.gene_count)]
         # 父母不为空, 基因重组
@@ -115,11 +124,23 @@ class Individual:
       
     def calc_fittness(self, debug=False):
         '''计算个体适应性, 适应性保证非负, 且适应性越高, 个体越不容易被淘汰'''
-        positions = np.array(UAV_start_position, dtype=int) # 无人机当前位置
-        timer = np.array(UAV_search_cost, dtype=int)        # 无人机当前飞行倒计时
-        step = np.zeros(UAV_num, dtype=int)                 # 无人机当前飞了多少步
-        map_vist = np.zeros((map_width, map_height), dtype=int)  # 地图, 记录当前位置是否被搜索到
+        positions = np.array(UAV_start_position) # 无人机当前位置
+        fly_timer = np.zeros(UAV_num)            # 无人机当前飞行倒计时
+        fly_distance = np.zeros((UAV_num, 2))    # 无人机当前飞行长度
+        step = np.zeros(UAV_num, dtype=int)      # 无人机当前飞了多少步
+        map_vist = np.zeros((map_width, map_height))  # 地图, 记录当前位置是否被搜索到
 
+        def check_position(position):
+            '''检测位置是否合法'''
+            if position[0] < 0 or position[0] >= map_width:
+                return False
+            if position[1] < 0 or position[1] >= map_height:
+                return False
+            print(position)
+            if map_time[position[0]][position[1]] == -1:
+                return False
+            return True
+        
         def update_map(time):
             '''
             更新地图
@@ -127,12 +148,6 @@ class Individual:
             '''
             # 判断是否有无人机相撞
             for i in range(UAV_num):
-                # 位置是否合法
-                if positions[i][0] < 0 or positions[i][0] >= map_width:
-                    return time - UAV_search_time
-                if positions[i][1] < 0 or positions[i][1] >= map_height:
-                    return time - UAV_search_time
-                # 是否与其他无人机相撞
                 for j in range(i):
                     if math.dist(positions[i], positions[j]) < safe_distance:
                         return time - UAV_search_time
@@ -151,24 +166,32 @@ class Individual:
         # 模拟搜索过程
         for time in range(UAV_search_time):
             for UAV_index in range(UAV_num):
-                timer[UAV_index] -= 1
-                if timer[UAV_index] == 0:
-                    # 无人机飞行一步
+                # 计时器为0, 根据策略选择飞行目标, 设置飞行倒计时
+                if fly_timer[UAV_index] == 0:
+                    # 飞行策略 => 飞行距离
                     option = self.genotype[self.gene_start_index[UAV_index] + step[UAV_index]]
+                    step[UAV_index] += 1
                     if option.data == 0: # 左转
-                        positions[UAV_index][0] += -1
-                        positions[UAV_index][1] += 0
+                        fly_distance[UAV_index] = np.array([-1, 0])
                     elif option.data == 1:  # 前进
-                        positions[UAV_index][0] += 0
-                        positions[UAV_index][1] += 1
+                        fly_distance[UAV_index] = np.array([0, 1])
                     elif option.data == 2:  # 右转
-                        positions[UAV_index][0] += 1
-                        positions[UAV_index][1] += 0
+                        fly_distance[UAV_index] = np.array([1, 0])
                     elif option.data == 3:  # 后退
-                        positions[UAV_index][0] += 0
-                        positions[UAV_index][1] += -1 
-                    # 重置计时器
-                    timer[UAV_index] = UAV_search_cost[UAV_index]
+                        fly_distance[UAV_index] = np.array([0, -1])
+                    # 计算目标位置
+                    positions[UAV_index] = np.ceil(positions[UAV_index])
+                    target = (positions[UAV_index] + fly_distance[UAV_index]).astype(int)
+                    # 目标位置不合法
+                    if check_position(target) == False:
+                        self.fittness = time - UAV_search_time
+                        return
+                    # 从地图中读取飞行时间
+                    fly_timer[UAV_index] = map_time[target[0]][target[1]]
+                # 前进一个单位时间
+                positions[UAV_index][0] += 1.0 / fly_timer[UAV_index] * fly_distance[UAV_index][0]
+                positions[UAV_index][1] += 1.0 / fly_timer[UAV_index] * fly_distance[UAV_index][1]
+                fly_timer[UAV_index] -= 1
             self.fittness = update_map(time)
             if self.fittness < 0:
                 return
